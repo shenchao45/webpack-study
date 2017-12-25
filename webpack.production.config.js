7d8b370a2922bffa204e62d29a7926084a8ae1d0
shenchao <shenchao@shouqianba.cn>
2017-12-25 18:35:51 +0800

    优化了一些插件，比如文件缓存，采用hash，每次清除之前的build之后的文件，js、css的压缩

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,11 +1,14 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");//因为有hash，防止每次都生产一个文件，文件过多
+
 module.exports = {
     devtool: 'null', //注意修改了这里，这能大大压缩我们的打包代码
     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/build",//打包后的文件存放的地方
-        filename: "bundle.js"//打包后输出文件的文件名
+        filename: "bundle-[hash].js"//打包后输出文件的文件名,缓存
     },
     devServer: {
         contentBase: "./public",//本地服务器所加载的页面所在的目录
@@ -44,6 +47,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
-        new webpack.HotModuleReplacementPlugin()//热加载插件
+        new webpack.HotModuleReplacementPlugin(),//热加载插件
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css"),
+        new CleanWebpackPlugin('build/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ]
 }
